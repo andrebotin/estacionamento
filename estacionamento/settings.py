9b6f7658434b4b4f6bf762ea7df30b2b7940fc6f
andrebotin <andrebotin@gmail.com>
2018-10-18 00:28:12 -0300

    Correção settings.py

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'https://estacionamento-test.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'estacionamento-test.herokuapp.com/']
 
 
 # Application definition
