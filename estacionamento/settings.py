4d8f50cd4d56dc452d481e7cee81724bf4bc34ca
andrebotin <andrebotin@gmail.com>
2018-10-20 20:47:41 -0300

    Atualização settings.py

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    ('../website/static'),
+    os.path.join('../website/static'),
 ]
 
 LOGIN_URL = 'login'
